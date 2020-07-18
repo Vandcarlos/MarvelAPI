e09cb8ada910c9382b1b9ee8d585d16942f201f6
Vandcarlos Mouzinho Sandes Junior <vandcarlosjunior@gmail.com>
2020-07-17 23:13:55 -0300

    Added basic tests to test github action

---
+++ Example/Tests/CharactersTests.swift
@@ -0,0 +1,23 @@
+//
+//  CharactersTests.swift
+//  MarvelAPI_Tests
+//
+//  Created by Vandcarlos Mouzinho Sandes Junior on 17/07/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class CharactersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+
+}
