aafe857485a1e52e70b5d1cf4ec37ed43cdfd201
Vandcarlos Mouzinho Sandes Junior <vandcarlosjunior@gmail.com>
2020-07-17 23:19:25 -0300

    Fixed tests script

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -workspace Example/MarvelAPI.xcworkspace \
-            -scheme MarvelAPI \
+            -scheme MarvelAPI-Example \
             -destination platform=iOS\ Simulator,name=iPhone\ 11 \
             clean test | xcpretty
